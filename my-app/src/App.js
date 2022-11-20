305a91e059afb864e1ecf9a7d3031aed9cd71606
tourdesegur <106815001+tourdesegur@users.noreply.github.com>
2022-11-20 05:40:51 +0000

    HTML/CSS start screen

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,28 @@
 
 import './App.css';
 
-function App() {
+function App(props) {
   return (
-    <div className="App">
-      <button className="btn btn-primary">Click</button>
+    <div className="App container">
+      <div className="container main-section">
+       
+        <h1 className='appName'><span className='logoLetter'>:D</span><span className='appTitle'>ictionary</span></h1>
+   
+       
+            <i class="bi bi-search"></i>
+            <form onSubmit={null}>
+              <input type='search' 
+              className='form-control'
+              placeholder='search here' 
+              autoFocus={true}
+              onChange={null}/>
+            </form>
+   
+      </div>
+      <small>Created by Viktoriia Perehuda,
+      <br />
+      GitHub code
+      </small>
     </div>
   );
 }
