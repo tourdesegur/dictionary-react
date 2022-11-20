10ea2a9307b6a87a80e4d314d3e98164b2d5f7bd
tourdesegur <106815001+tourdesegur@users.noreply.github.com>
2022-11-20 03:47:40 +0000

    React App created, BS, axios

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <button className="btn btn-primary">Click</button>
+    </div>
+  );
+}
+
+export default App;
